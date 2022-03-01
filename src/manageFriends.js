2948845b0361b3f8d8171033eed2dcc4b4bd525d
avcibatuhan <avcibatuhan26@gmail.com>
2022-03-01 10:30:11 +0300

    redux reducers lab

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,21 @@
-export function manageFriends(state, action) {
+const initialState = {
+  friends: [],
+}
+
+export function manageFriends(state=initialState, action) {
   // your code here
+  switch (action.type) {
+    case 'friends/add':
+      return {
+        ...state,
+        friends: [...state.friends,action.payload],
+      }
+    case "friends/remove":
+        return {
+          ...state,
+          friends:state.friends.filter((friend) => friend.id !== action.payload),
+        }
+    default:
+      return state;
+  }
 }
